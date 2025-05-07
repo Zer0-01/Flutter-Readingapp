aeb8c8a8d47ee99902c39531cce7845ce36f9483
Anas <anas.zulkifli2001@gmail.com>
2025-05-07 22:21:09 +0800

    feat: update strings

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:readingapps/extensions.dart';
 
@@ -15,12 +16,44 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       body: Column(
+        spacing: 8,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             color: Colors.grey,
             width: context.screenWidth,
             height: context.heightPct(20),
           ),
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () => context.router.pushPath("/reading"),
+                child: Container(
+                  width: context.widthPct(50),
+                  height: context.heightPct(20),
+                  color: Colors.blue,
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    context.loc.reading,
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () => context.router.pushPath("/reading"),
+                child: Container(
+                  width: context.widthPct(50),
+                  height: context.heightPct(20),
+                  color: Colors.red,
+                  alignment: Alignment.bottomCenter,
+                  child: Text(
+                    context.loc.fonics,
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
