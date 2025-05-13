2f2126c8310ce5af102f36926e118ee0c9fd6dc7
Anas <anas.zulkifli2001@gmail.com>
2025-05-14 00:39:24 +0800

    feat: add audio

--- lib/presentation/fonic/fonic_screen.dart
+++ lib/presentation/fonic/fonic_screen.dart
@@ -1,6 +1,7 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:readingapps/constants.dart';
+import 'package:readingapps/data/models/phonic.dart';
 import 'package:readingapps/extensions.dart';
 import 'package:readingapps/functions.dart';
 
@@ -9,34 +10,21 @@ class FonicScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> fonicList = [
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "E'",
-      "F",
-      "G",
-      "H",
-      "I",
-      "J",
-      "K",
-      "L",
-      "M",
-      "N",
-      "O",
-      "P",
-      "Q",
-      "R",
-      "S",
-      "T",
-      "U",
-      "V",
-      "W",
-      "X",
-      "Y",
-      "Z"
+    List<Phonic> phonics = [
+      Phonic(name: "A", audio: AudioConstants.AUDIO_A),
+      Phonic(name: "B", audio: AudioConstants.AUDIO_B),
+      Phonic(name: "C", audio: AudioConstants.AUDIO_C),
+      Phonic(name: "D", audio: AudioConstants.AUDIO_D),
+      Phonic(name: "E", audio: AudioConstants.AUDIO_E),
+      Phonic(name: "E'", audio: AudioConstants.AUDIO_E_2),
+      Phonic(name: "F", audio: AudioConstants.AUDIO_F),
+      Phonic(name: "G", audio: AudioConstants.AUDIO_G),
+      Phonic(name: "H", audio: AudioConstants.AUDIO_H),
+      Phonic(name: "I", audio: AudioConstants.AUDIO_I),
+      Phonic(name: "J", audio: AudioConstants.AUDIO_J),
+      Phonic(name: "K", audio: AudioConstants.AUDIO_K),
+      Phonic(name: "L", audio: AudioConstants.AUDIO_L),
+      Phonic(name: "M", audio: AudioConstants.AUDIO_M),
     ];
 
     return Scaffold(
@@ -49,12 +37,12 @@ class FonicScreen extends StatelessWidget {
             crossAxisCount: 4,
             mainAxisSpacing: 4,
             crossAxisSpacing: 4,
-            children: fonicList
+            children: phonics
                 .map(
-                  (fonic) => GestureDetector(
+                  (phonic) => GestureDetector(
                     onTap: () {
                       final audioPlayer = AudioPlayer();
-                      audioPlayer.play(AssetSource(AudioConstants.AUDIO_A));
+                      audioPlayer.play(AssetSource(phonic.audio));
                     },
                     child: Container(
                       decoration: BoxDecoration(
@@ -63,7 +51,7 @@ class FonicScreen extends StatelessWidget {
                       ),
                       alignment: Alignment.center,
                       child: Text(
-                        fonic,
+                        phonic.name,
                         style: const TextStyle(
                             fontSize: 30, fontWeight: FontWeight.bold),
                       ),
