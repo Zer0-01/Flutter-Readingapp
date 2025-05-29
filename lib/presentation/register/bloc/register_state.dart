0e4cc44257e1880825111a39454e3bffc7921745
Anas <anas.zulkifli2001@gmail.com>
2025-05-29 23:27:46 +0800

    feat: implement loading

--- lib/presentation/register/bloc/register_state.dart
+++ lib/presentation/register/bloc/register_state.dart
@@ -1,20 +1,31 @@
 part of 'register_bloc.dart';
 
+enum RegisterStatus {
+  initial,
+  loading,
+  success,
+  failure,
+}
+
 class RegisterState extends Equatable {
+  final RegisterStatus registerStatus;
   final bool isObscureText;
 
   const RegisterState({
+    this.registerStatus = RegisterStatus.initial,
     this.isObscureText = false,
   });
 
   RegisterState copyWith({
+    RegisterStatus? registerStatus,
     bool? isObscureText,
   }) {
     return RegisterState(
+      registerStatus: registerStatus ?? this.registerStatus,
       isObscureText: isObscureText ?? this.isObscureText,
     );
   }
 
   @override
-  List<Object?> get props => [isObscureText];
+  List<Object?> get props => [registerStatus, isObscureText];
 }
--- lib/presentation/register/register_setup_screen.dart
+++ lib/presentation/register/register_setup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:readingapps/data/repository/auth_repository.dart';
 import 'package:readingapps/presentation/register/bloc/register_bloc.dart';
 import 'package:readingapps/presentation/register/register_screen.dart';
 
@@ -11,6 +12,7 @@ class RegisterSetupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-        create: (context) => RegisterBloc(), child: RegisterScreen());
+        create: (context) => RegisterBloc(authRepository: AuthRepository()),
+        child: const RegisterScreen());
   }
 }
